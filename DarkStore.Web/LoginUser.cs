5fdb779f265b513ff53c783e7f972c623b702234
Raquel Silva <86536901+raqueldcsilva@users.noreply.github.com>
2023-07-28 15:41:29 -0300

    adding item to the shopping cart

---
+++ DarkStore.Web/LoginUser.cs
@@ -0,0 +1,7 @@
+namespace DarkStore.Web;
+//Essa classe foi criada apenas porque não elaboramos um servico de autenticacao
+public class LoginUser
+{
+    public const int UserId = 1;
+    public const int ShoppingCartId = 1;
+}
